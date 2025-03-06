8535f458217198f96bf842c6afd91f6681612e81
luisJaimeB <luisyi1998@gmail.com>
2025-03-06 11:59:18 -0500

    feat: scss and bootstrap are added.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+import $ from 'jquery'
+window.$ = $
+
+import './assets/css/style.css'
+import './assets/js/main.js'
 
 const app = createApp(App)
 
