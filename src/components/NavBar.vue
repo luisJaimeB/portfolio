56ebeace0a0c40bfd860ebdfe804737b751e5aa7
luisJaimeB <luisyi1998@gmail.com>
2025-03-06 11:12:56 -0500

    feat: navbar are added.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,96 @@
+<template>
+        <header class="tmp-header-area-start header-one header--sticky header--transparent">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-content">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img class="logo-dark" src="../assets/images/logo/white-logo-reeni-3.png" alt="Reeni - Personal Portfolio HTML Template for developers and freelancers">
+                                <img class="logo-white" src="../assets/images/logo/logo-white.png" alt="Reeni - Personal Portfolio HTML Template for developers and freelancers">
+                            </a>
+                        </div>
+                        <nav class="tmp-mainmenu-nav d-none d-xl-block">
+                            <ul class="tmp-mainmenu">
+                                <li>
+                                    <a href="#">Home
+                                    </a>
+                                    <!-- <ul class="submenu">
+            <li><a href="index.html">Home 01</a></li>
+            <li><a href="index-02.html">Home 02</a></li>
+            <li><a href="index-03.html">Home 03</a></li>
+            <li><a href="index-04.html">Home 04</a></li>
+            <li><a href="index-05.html">Home 05</a></li>
+            <li><a href="index-06.html">Home 06</a></li>
+            <li><a href="index-07.html">Home 07</a></li>
+            <li><a href="index-08.html">Home 08</a></li>
+            <li><a href="index-09.html">Home 09</a></li>
+            <li><a href="index-10.html">Home 10</a></li>
+            <li><a href="index-11.html">Home 11</a></li>
+            <li><a href="index-12.html">Home 12</a></li>
+            <li><a href="index-13.html">Home 13</a></li>
+        </ul> -->
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li class="has-dropdown">
+                                    <a href="#">Services
+                                        <i class="fa-regular fa-chevron-down"></i>
+                                    </a>
+                                    <ul class="submenu">
+                                        <li><a href="service.html">Service</a></li>
+                                        <li><a href="service-details.html">Service Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-dropdown">
+                                    <a href="#">Blog
+                                        <i class="fa-regular fa-chevron-down"></i>
+                                    </a>
+                                    <ul class="submenu">
+                                        <li><a href="blog.html">Blog Classic</a></li>
+                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="has-dropdown">
+                                    <a href="#">Project
+                                        <i class="fa-regular fa-chevron-down"></i>
+                                    </a>
+                                    <ul class="submenu">
+                                        <li><a href="project.html">Project</a></li>
+                                        <li><a href="project-details.html">Project Details</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+
+                        </nav>
+                        <div class="tmp-header-right">
+                            <div class="social-share-wrapper d-none d-md-block">
+                                <div class="social-link">
+                                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                                </div>
+                            </div>
+                            <div class="actions-area">
+                                <div class="tmp-side-collups-area d-none d-xl-block">
+                                    <button class="tmp-menu-bars tmp_button_active"><i class="fa-regular fa-bars-staggered"></i></button>
+                                </div>
+                                <div class="tmp-side-collups-area d-block d-xl-none">
+                                    <button class="tmp-menu-bars humberger_menu_active"><i class="fa-regular fa-bars-staggered"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>    
+</template>
+<script>
+
+</script>
